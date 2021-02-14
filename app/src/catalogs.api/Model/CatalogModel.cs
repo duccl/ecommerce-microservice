433d8cd41c74fcd4e7c34f98f7f7a788a38b6e34
ecoltri2 <eduardo.coltri@dxc.com>
2021-02-14 17:51:20 -0300

    Catalog Model added

---
+++ app/src/catalogs.api/Model/CatalogModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catalogs.api.Model
+{
+    public class CatalogModel
+    {
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
