d848c931e5cec429c4089add1d91b375311fc146
ecoltri2 <eduardo.coltri@dxc.com>
2021-02-14 19:13:42 -0300

    product entity added

---
+++ app/src/catalogs.api/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace catalogs.api.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        [MinLength(2)]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Summary { get; set; }
+        [Range(0,int.MaxValue)]
+        public int Quantity { get; set; }
+        [Range(0.00,double.MaxValue)]
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
