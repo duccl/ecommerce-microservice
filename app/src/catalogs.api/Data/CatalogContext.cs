648e83b1fff1438c674ec0fb8d1d8160c7faa5ba
ecoltri2 <edr.ccol@gmail.com>
2021-02-15 09:08:36 -0300

    Catalog context added

---
+++ app/src/catalogs.api/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using catalogs.api.Data.Interfaces;
+using MongoDB.Driver;
+using catalogs.api.Entities;
+using catalogs.api.Settings;
+namespace catalogs.api.Data
+{
+    public class CatalogContext: ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+        public CatalogContext(ICatalogDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            Products = database.GetCollection<Product>(databaseSettings.CollectionName);
+        }
+    }
+}
