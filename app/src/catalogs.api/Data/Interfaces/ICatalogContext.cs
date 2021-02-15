90cf0d3046015161ac90d2eee4c77294cd8d1ce8
ecoltri2 <edr.ccol@gmail.com>
2021-02-15 08:53:36 -0300

    Catalog Context added

---
+++ app/src/catalogs.api/Data/Interfaces/ICatalogContext.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+using catalogs.api.Entities;
+namespace catalogs.api.Data.Interfaces
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
