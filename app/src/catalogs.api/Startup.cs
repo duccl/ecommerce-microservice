96edefdc0d02268b2cd325088db30550e4352ef7
ecoltri2 <edr.ccol@gmail.com>
2021-02-15 08:47:05 -0300

    Catalog Database injected at service

--- app/src/catalogs.api/Startup.cs
+++ app/src/catalogs.api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -23,18 +24,27 @@ namespace catalogs.api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "catalogs.api", Version = "v1" });
             });
+
+            // Injetando a classe Catalog
+            services.Configure<Settings.CatalogDatabaseSettings>(
+                Configuration.GetSection(nameof(Settings.CatalogDatabaseSettings))
+            );
+
+            // Cadastrando nossa interface
+            // Basicamente ele entende que toda classe que herdar a interface, deverá executar o seguinte comando
+            // Como é um singleton, apenas a única instância executará o código
+            services.AddSingleton<Settings.ICatalogDatabaseSettings>(
+                serviceProvider => serviceProvider.GetRequiredService<IOptions<Settings.CatalogDatabaseSettings>>().Value
+            );
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
