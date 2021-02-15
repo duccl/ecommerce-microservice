8db4be6bb9160ca0eb8b028b2ec0ecbc6341b633
ecoltri2 <edr.ccol@gmail.com>
2021-02-15 08:31:26 -0300

    libs removed

--- app/src/catalogs.api/Settings/ICatalogDatabaseSettings.cs
+++ app/src/catalogs.api/Settings/ICatalogDatabaseSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace catalogs.api.Settings
+﻿namespace catalogs.api.Settings
 {
     public interface ICatalogDatabaseSettings
     {
