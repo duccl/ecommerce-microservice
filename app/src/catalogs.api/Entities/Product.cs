58a9c5371c0947d8e7cc70270940bdd1aec1ca48
ecoltri2 <edr.ccol@gmail.com>
2021-02-16 11:10:39 -0300

    Data Annotations at product changed

--- app/src/catalogs.api/Entities/Product.cs
+++ app/src/catalogs.api/Entities/Product.cs
@@ -9,13 +9,17 @@ namespace catalogs.api.Entities
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; }
         [MinLength(2)]
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Category { get; set; }
         public string Description { get; set; }
         public string Summary { get; set; }
         [Range(0,int.MaxValue)]
+        [Required]
         public int Quantity { get; set; }
         [Range(0.00,double.MaxValue)]
+        [Required]
         public decimal Price { get; set; }
         public string Image { get; set; }
     }
