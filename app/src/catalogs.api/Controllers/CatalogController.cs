8b4fccd477a782529a6f39f596578e1c2c9b8fe0
ecoltri2 <edr.ccol@gmail.com>
2021-02-15 22:57:38 -0300

    GetProducts Action added

--- app/src/catalogs.api/Controllers/CatalogController.cs
+++ app/src/catalogs.api/Controllers/CatalogController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using catalogs.api.Entities;
+using catalogs.api.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace catalogs.api
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class CatalogController : ControllerBase
-    {
+    { 
+        private readonly IProductRepository _repository;
+
+        public CatalogController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductsAsync()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
     }
 }
