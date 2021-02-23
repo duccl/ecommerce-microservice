240869d35efb449056032b33e929ef9f135fef92
duccl <edr.ccol@gmail.com>
2021-02-22 22:54:12 -0300

    loggin if env is development

--- app/src/catalogs.api/Startup.cs
+++ app/src/catalogs.api/Startup.cs
@@ -9,17 +9,19 @@ using catalogs.api.Data;
 using catalogs.api.Data.Interfaces;
 using catalogs.api.Repositories.Interfaces;
 using catalogs.api.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace catalogs.api
 {
     public class Startup
     {
+        public ILogger<Startup> _logger;
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -28,7 +30,7 @@ namespace catalogs.api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "catalogs.api", Version = "v1" });
             });
-            
+
             // Cadastrando nossa interface
             // Basicamente ele entende que toda classe que herdar a interface, devera executar o seguinte comando
             // Como eh um singleton, apenas a unica instancia executara o codigo
@@ -38,8 +40,10 @@ namespace catalogs.api
             services.AddTransient<IProductRepository, ProductRepository>();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            _logger = logger;
+            _logger.LogInformation($"Up and running with env with development as {env.IsDevelopment()}");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
