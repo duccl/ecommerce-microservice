95f878941bf07576571301fb6851d830cfbab172
duccl <edr.ccol@gmail.com>
2021-04-03 00:37:26 -0300

    removing ctor binding catalog settings

--- app/src/catalogs.api/Settings/CatalogDatabaseSettings.cs
+++ app/src/catalogs.api/Settings/CatalogDatabaseSettings.cs
@@ -7,11 +7,6 @@ namespace catalogs.api.Settings
         public string CollectionName { get; set; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
-        public CatalogDatabaseSettings()
-        {
-            CollectionName = Environment.GetEnvironmentVariable("COLLECTIONNAME");
-            ConnectionString = Environment.GetEnvironmentVariable("CONNECTIONSTRING");
-            DatabaseName = Environment.GetEnvironmentVariable("DATABASENAME");
-        }
+        
     }
 }
