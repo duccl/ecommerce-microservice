9ec60a454c5fa6c3b2a528835c89d57201e03a05
duccl <edr.ccol@gmail.com>
2021-04-03 00:34:27 -0300

    adding database config extensions method

---
+++ app/src/settings/Database/DatabaseServiceExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+namespace Settings.Database
+{
+    public static class DatabaseServiceExtension
+    {
+        public static IServiceCollection AddDatabaseBinding<TInterface, TClass>( this IServiceCollection services, 
+                                                              IConfiguration configuration) where TClass: new()
+        {
+            var settingsObject = new TClass();
+            var section =configuration.GetSection(typeof(TClass).Name); 
+            section.Bind(settingsObject);
+            services.AddSingleton(typeof(TInterface),settingsObject);
+            return services;
+        }
+
+        public static IServiceCollection AddDatabaseBinding<TClass>(IServiceCollection services, 
+                                                              IConfiguration configuration) where TClass: new()
+        {
+            var settingsObject = new TClass();
+            var section =configuration.GetSection(typeof(TClass).Name); 
+            section.Bind(settingsObject);
+            services.AddSingleton(typeof(TClass));
+            return services;
+        }
+    }
+}
