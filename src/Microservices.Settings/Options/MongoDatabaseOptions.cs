1e09995180a6bb387e134d09d987ef5f98ce23fb
duccl <edr.ccol@gmail.com>
2021-05-30 14:19:11 -0300

    settings project added

---
+++ src/Microservices.Settings/Options/MongoDatabaseOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Microservices.Settings.Options
+{
+    public class MongoDatabaseOptions
+    {
+        public const string MongoDatabaseSection = "MongoSettings";
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
