b32ec0bfaa03bd79e4d9fddbb91410079509655a
duccl <edr.ccol@gmail.com>
2021-05-30 01:20:19 -0300

    app folder removed

--- app/src/catalogs.api/Data/Interfaces/ICatalogContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Driver;
-using catalogs.api.Entities;
-namespace catalogs.api.Data.Interfaces
-{
-    public interface ICatalogContext
-    {
-        IMongoCollection<Product> Products { get; }
-    }
-}
