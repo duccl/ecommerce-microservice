496ccf9e40f1c8a2fdf5cf5937a865206c13291b
duccl <edr.ccol@gmail.com>
2021-07-04 14:11:26 -0300

    new basket entities and packages dependencies added

---
+++ src/Basket/Microservices.Basket.Domain/Entities/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Microservices.Basket.Domain.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public IEnumerable<ShoppingCartItem> Items { get; set; }
+        public decimal TotalPrice {
+            get {
+                return Items.Sum(item => item.TotalPrice);
+            }
+        }
+    }
+}
---
+++ src/Basket/Microservices.Basket.Domain/Entities/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Microservices.Basket.Domain.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice => Quantity * Price;
+    }
+}
